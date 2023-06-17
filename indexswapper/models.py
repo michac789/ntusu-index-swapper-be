5da461f93d32ad23b093897217094e40e0549d1f
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-17 18:35:21 +0800

    [IndexSwapper] add CourseIndex & SwapRequest model and migration

--- indexswapper/models.py
+++ indexswapper/models.py
@@ -1,3 +1,70 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from sso.models import User
 
-# Create your models here.
+
+class CourseIndex(models.Model):
+    code = models.CharField(max_length=6)
+    name = models.CharField(max_length=100)
+    academic_units = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    index = models.CharField(max_length=5, unique=True)
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    information = models.TextField()
+    pending_count = models.IntegerField(default=0)
+
+    @property
+    def get_information(self):
+        def serialize(msg):
+            a = msg.split('^')
+            return {
+                'type': a[0], 'group': a[1],
+                'day': a[2], 'time': a[3],
+                'venue': a[4], 'remark': a[5]
+            }
+        return [serialize(x) for x in self.information.split(';')]
+
+    class Meta:
+        verbose_name_plural = 'Course Indexes'
+
+    def __str__(self):
+        return f'<Course Code {self.code}, Index {self.index}>'
+
+
+class SwapRequest(models.Model):
+    class Status(models.TextChoices):
+        SEARCHING = 'S', 'Searching'
+        WAITING = 'W', 'Waiting'
+        COMPLETED = 'C', 'Completed'
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='swap_requests'
+    )
+    contact_info = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.SEARCHING
+    )
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    datetime_found = models.DateTimeField(blank=True, null=True)
+    current_index = models.ForeignKey(
+        CourseIndex, on_delete=models.SET_NULL,
+        related_name='available_swap', null=True
+    )
+    wanted_indexes = models.CharField(max_length=100)
+
+    @property
+    def get_wanted_indexes(self):
+        return self.wanted_indexes.split(';')
+
+    class Meta:
+        verbose_name_plural = 'Swap Requests'
+
+    def __str__(self):
+        return f'''<Swap Request by '{self.user.username}':
+            {self.current_index.code} ({self.current_index.index}
+            to {self.wanted_indexes})>
+        '''
