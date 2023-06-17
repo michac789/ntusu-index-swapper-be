9830f43d7d51d634109360a0bac690462458dbf1
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-17 15:00:28 +0800

    [Setup] add cors headers config

--- NTUSU_BE/settings.py
+++ NTUSU_BE/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
 
     'indexswapper',
     'sso',
@@ -49,6 +50,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+
+    'corsheaders.middleware.CorsMiddleware',
+
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -143,3 +147,10 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+CORS_ALLOW_HEADERS = [
+    "Content-Type", "Authorization"
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
