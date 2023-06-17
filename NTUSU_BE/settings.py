d6a9a163455c852ca356531e0429d3ca523649dc
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-17 14:53:43 +0800

    [SSO] create User model + migration + setup admin

--- NTUSU_BE/settings.py
+++ NTUSU_BE/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Additional settings
+
+AUTH_USER_MODEL = 'sso.User'
