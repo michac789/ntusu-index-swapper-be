de1582dbf08dd442d4f9dfbff75f388bd9320c4c
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-17 14:53:59 +0800

    [SSO] setup jwt token auth

--- sso/urls.py
+++ sso/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 app_name = 'sso'
-urlpatterns = []
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
+]
