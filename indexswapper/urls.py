d740ea6ac6fefd6c3023b11e937bd34a56c6b146
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-18 14:41:30 +0800

    [IndexSwapper] added viewsets to see all courses & indexes based on course

--- indexswapper/urls.py
+++ indexswapper/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from indexswapper.views import PopulateDatabaseView
+from rest_framework import routers
+from indexswapper.views import PopulateDatabaseView, CourseIndexViewSet
 
 
 app_name = 'indexswapper'
+router = routers.DefaultRouter()
+router.register('courseindex', CourseIndexViewSet, basename='courseindex')
+
 urlpatterns = [
     path('populate_db/',
          PopulateDatabaseView.as_view(), name='populate_db')
-]
+] + router.urls
--- indexswapper/views.py
+++ indexswapper/views.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from indexswapper.models import CourseIndex
 from indexswapper.serializers import PopulateDatabaseSerializer
 from indexswapper.utils.scraper import populate_modules
 
@@ -21,3 +25,28 @@ class PopulateDatabaseView(GenericAPIView):
             })
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CourseIndexViewSet(ViewSet):
+    @action(methods=['get'], detail=False)
+    def get_courses(self, *args, **kwargs):
+
+        # TODO - not supported on sqlite, change to mysql or postgres soon!
+        # instances = CourseIndex.objects.all().distinct('code')
+
+        instances = CourseIndex.objects.values('code', 'name')
+        unique_course_dict = defaultdict()
+        for instance in instances:
+            unique_course_dict[instance['code']] = instance['name']
+        return Response([{
+            'code': code, 'name': name
+        } for code, name in unique_course_dict.items()])
+
+    @action(methods=['get'], detail=False, url_name='get_indexes',
+            url_path='get_indexes/(?P<course_code>[^/.]+)')
+    def get_indexes_from_course(self, *args, **kwargs):
+        instances = CourseIndex.objects.filter(code=kwargs['course_code'])
+        return Response([{
+            'index': x.index, 'pending_count': x.pending_count,
+            'information': x.get_information
+        } for x in instances])
